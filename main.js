ff77cdc554d9793fc794def6648721fa620b1e78
TheProCoder18 <91504105+TheProCoder18@users.noreply.github.com>
2022-10-01 17:20:11 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+img="";
+objects = [];
+status = "";
+
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+
+function setup() {
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    video.hide();
+}
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+
+    fill("#ff0000");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#ff0000");
+    rect(30, 60, 450, 350);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+
+    if(status != "")
+{
+    r = random(255);
+    g = random(255);
+    b = random(255);
+    objectDetector.detect(video, gotResult);
+    for (i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :";
+        
+
+        fill(r,g,b);
+
+        fill("FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x, objects[i].y);
+        noFill();
+        stroke(r,g,b)
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+}
+
+
+
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = " Status : Detecting Objects";
+}
